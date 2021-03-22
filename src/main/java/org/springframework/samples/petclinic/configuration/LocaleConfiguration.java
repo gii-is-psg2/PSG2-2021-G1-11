a00783ec5c6b24f9541b0b93e81bc530c166b367
adria <adria@192.168.1.139>
2021-03-22 13:17:28 +0100

    Complete localization to Spanish

---
+++ src/main/java/org/springframework/samples/petclinic/configuration/LocaleConfiguration.java
@@ -0,0 +1,60 @@
+/*
+ * LocaleConfiguration.java
+ *
+ * Copyright (c) 2012-2021 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package org.springframework.samples.petclinic.configuration;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfiguration implements WebMvcConfigurer {
+
+	// WebMvcConfigurer interface ---------------------------------------------
+
+	@Override
+	public void addInterceptors(final InterceptorRegistry registry) {
+		registry.addInterceptor(this.localeChangeInterceptor());
+	}
+
+	// Beans ------------------------------------------------------------------
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:locale/messages");
+		messageSource.setCacheSeconds(3600);
+		return messageSource;
+
+	}
+
+	@Bean
+	public LocaleContextResolver localeResolver() {
+		final SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		final LocaleChangeInterceptor result = new LocaleChangeInterceptor();
+		result.setParamName("lang");
+		return result;
+	}
+
+}
