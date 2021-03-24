256a33c554be0c555161022920194de9bb31144e
Pedro Alonso Pontiga <pedropontiga@gmail.com>
2021-03-24 17:44:04 +0100

    Create Speciality service and repository

---
+++ src/main/java/org/springframework/samples/petclinic/repository/SpecialtyRepository.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.repository;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.repository.Repository;
+import org.springframework.samples.petclinic.model.Specialty;
+
+public interface SpecialtyRepository extends Repository<Specialty, Integer> {
+	void save(Specialty specialty) throws DataAccessException;
+
+	Specialty findById(int id) throws DataAccessException;
+
+	Specialty findByName(String name) throws DataAccessException;
+}
