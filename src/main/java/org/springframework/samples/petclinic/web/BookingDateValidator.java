2cc6ae965c44dc5b2b47508b8ed5e0afd4f57e1b
Guillermo Diz <GuillermoX7@gmail.com>
2021-04-02 12:59:04 +0200

    Added internationalization for Booking validator messages

--- src/main/java/org/springframework/samples/petclinic/web/BookingDateValidator.java
+++ src/main/java/org/springframework/samples/petclinic/web/BookingDateValidator.java
@@ -11,36 +11,32 @@ public class BookingDateValidator implements Validator {
 
 	@Override
 	public void validate(Object obj, Errors errors) {
-
 		Booking booking = (Booking) obj;
 		String startDate = booking.getStartDate();
 		String finishDate = booking.getFinishDate();
 		LocalDate today = LocalDate.now();
 
 		if (startDate.isBlank()) {
-			errors.rejectValue("startDate", "Required field", "Required field");
+			errors.rejectValue("startDate", "required");
 		} else {
 			LocalDate date;
 			try {
 				date = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 				if (date.isBefore(today)) {
-					errors.rejectValue("startDate", "Start date must be greater than today's date",
-							"Start date must be greater than today's date");
+					errors.rejectValue("startDate", "dateBeforeToday");
 				}
 			} catch (Exception e) {
-				errors.rejectValue("startDate", "Incorrect date format. It must be yyyy/mm/dd",
-						"Incorrect date format. It must be yyyy/mm/dd");
+				errors.rejectValue("startDate", "invalidDateFormat");
 			}
 		}
 
 		if (finishDate.isBlank()) {
-			errors.rejectValue("finishDate", "Required field", "Required field");
+			errors.rejectValue("finishDate", "required");
 		} else {
 			try {
 				LocalDate.parse(finishDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 			} catch (Exception e) {
-				errors.rejectValue("finishDate", "Incorrect date format. It must be yyyy/mm/dd",
-						"Incorrect date format. It must be yyyy/mm/dd");
+				errors.rejectValue("finishDate", "invalidDateFormat");
 			}
 		}
 
@@ -48,8 +44,7 @@ public class BookingDateValidator implements Validator {
 			LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 			LocalDate end = LocalDate.parse(finishDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 			if (start.isAfter(end)) {
-				errors.rejectValue("finishDate", "End date must be greater than Start date",
-						"End date must be greater than Start date");
+				errors.rejectValue("finishDate", "endAfterStart");
 			}
 		}
 	}
